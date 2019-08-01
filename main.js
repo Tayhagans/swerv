5bfa65b85410d6659bad53aeea5a1bfd2fc7a012
Taylor Hagans <tayhagans@gmail.com>
2019-08-01 11:20:58 -0400

    update

--- main.js
+++ main.js
@@ -84,7 +84,7 @@ Array.prototype.forEach.call (inputs, function( input )
 }); 
 
 // Set the date we're counting down to
-var countDownDate = new Date("April 13, 2019 10:00:00").getTime();
+var countDownDate = new Date("April 11, 2020 10:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
